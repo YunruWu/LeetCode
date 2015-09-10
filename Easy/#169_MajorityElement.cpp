0c0245dc32c85077eece482f05586d4911beccf6
Vera Wu <VW@Veras-MacBook-Pro.local>
2015-09-09 22:49:30 -0700

    easy

---
+++ Easy/#169_MajorityElement.cpp
@@ -0,0 +1,28 @@
+/*
+Hash table
+O(n)
+ */
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> counts; 
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+            if (++counts[nums[i]] > n / 2)
+                return nums[i];
+    }
+};
+
+/*
+Sort
+O(nlogn)
+ */
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        return nums[nums.size() / 2];
+    } 
+};
