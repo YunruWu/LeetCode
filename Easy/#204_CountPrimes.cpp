5a9658992655e587753a54f505c27dfd41b7e53d
Vera Wu <VW@Veras-MacBook-Pro.local>
2015-09-14 12:47:36 -0700

    easy

---
+++ Easy/#204_CountPrimes.cpp
@@ -0,0 +1,45 @@
+/*
+O(n^1.5)
+ */
+class Solution {
+public:
+    int countPrimes(int n) {
+        bool isPrime[n];
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+        
+        // Loop's ending condition is i * i < n instead of i < sqrt(n)
+        // to avoid repeatedly calling an expensive function sqrt().
+        for (int i = 2; i * i < n; i++) {
+            if (!isPrime[i]) continue;
+            for (int j = i * i; j < n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+        
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) count++;
+        }
+        
+        return count;
+    }
+};
+
+
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool> prime(n, true);
+        prime[0] = false, prime[1] = false;
+        for (int i = 0; i < sqrt(n); ++i) {
+            if (prime[i]) {
+                for (int j = i*i; j < n; j += i) {
+                    prime[j] = false;
+                }    
+            }    
+        }
+        return count(prime.begin(), prime.end(), true);
+    }
+};
