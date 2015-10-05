e6405c4699b293aa1da8b7101471d3636bdee617
VeraWu <VW@Veras-MacBook-Pro.local>
2015-10-04 17:53:02 -0700

    1

--- Easy/#101_SymmetricTree.cpp
+++ Easy/#101_SymmetricTree.cpp
@@ -56,8 +56,8 @@ public:
 	}
 
 	bool isSymmetric_helper(TreeNode* root1, TreeNode* root2) {
-    	if (root1==NULL && root2==NULL) return true;
-    	if (root1==NULL || root2==NULL) return false;
+    	if (root1 == NULL && root2 == NULL) return true;
+    	if (root1 == NULL || root2 == NULL) return false;
     	if (root1->val != root2->val) return false;
     	return isSymmetric_helper(root1->left, root2->right) && isSymmetric_helper(root1->right, root2->left);
 	}
